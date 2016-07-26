d14a78686a3db3d0c462d7cc4744eecb7d9dbda0
Kenji Rikitake <kenji.rikitake@acm.org>
2016-07-26 18:39:59 +0900

    unix_efile.c: set READAHEAD_PAGES to 4096

--- erts/emulator/drivers/unix/unix_efile.c
+++ erts/emulator/drivers/unix/unix_efile.c
@@ -1000,7 +1000,7 @@ efile_sendfile(Efile_error* errInfo, int in_fd, int out_fd,
 	retval = sendfile(in_fd, out_fd, *offset, SENDFILE_CHUNK_SIZE,
 			  NULL, &len,
 #if defined(__FreeBSD__) && (__FreeBSD_version >= 1100000) /* FreeBSD 11 */
-#define READAHEAD_PAGES (16)
+#define READAHEAD_PAGES (4096)
 			  SF_FLAGS(READAHEAD_PAGES, SF_NOCACHE));
 #else
 			  0);
